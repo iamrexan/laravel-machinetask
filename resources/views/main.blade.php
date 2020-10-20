2069042563b4c1ff9505531617606d00b5f390ff
Anto Rex <anto.rex@reflectionsinfos.com>
2020-10-21 00:26:17 +0530

    second stage nav bar added.

---
+++ resources/views/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webdura Main</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container-fluid my-5 custom-container">
+		<nav class="navbar cardheader-custom mx-auto">
+			<div class="d-inline-block">
+				<ul class="nav navbar-nav d-inline-block">
+					<li class="list-inline-item mx-lg-4 mx-md-4"><i class="fa fa-chevron-left"></i></li>
+					<li class="list-inline-item mx-lg-4 mx-md-4">Requests</li>
+					<li class="list-inline-item mx-lg-4 mx-md-4">Services</li>
+					<li class="list-inline-item mx-lg-4 mx-md-4">Payments</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="card">
+			<div class="cord-body">
+				
+			</div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="/js/app.js"></script>
+</html>
